5ec30f10c5058ea43704772605311f3c1d830f6a
konkovev-cyber <konkovev@gmail.com>
2026-02-17 19:19:09 +0300

    Initialize Capacitor and add Android platform for APK

---
+++ android/app/src/main/java/ru/slp23/app/MainActivity.java
@@ -0,0 +1,5 @@
+package ru.slp23.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ru.slp23.app',
+  appName: 'Личность ПЛЮС',
+  webDir: 'dist'
+};
+
+export default config;
